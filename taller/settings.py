b7d38448a73b20e8d38d6c0c655c18602c9b5fe6
Angelo Ruiz <omora3404@gmail.com>
2025-10-27 22:52:48 -0300

    Actualizar ALLOWED_HOSTS para acceso externo

--- taller/settings.py
+++ taller/settings.py
@@ -32,7 +32,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
